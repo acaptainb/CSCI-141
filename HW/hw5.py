627bf73e38fa5ea0e2fd73023ac1245a490168b8
A <a.captain.b@gmail.com>
2025-09-23 20:58:34 -0400

    update

---
+++ HW/hw5.py
@@ -0,0 +1,15 @@
+'''
+Abdulaziz
+'''
+def duplicate_char(in_string:str, character:str)->str:
+    pass
+
+def swap_parts(in_string:str, position:int)->str:
+    pass
+
+def count_occurrences(filename:str, target:str)->int:
+    pass
+
+if __name__() == '__main__':
+    #
+    pass
